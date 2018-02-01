ba2b7d0aaee1379486fd7ff5cf54ab30d3ee8696
EinBaum <mail@einbaum.org>
2018-02-01 21:44:25 +0100

    better class buffs

--- ChroniclesPTR.lua
+++ ChroniclesPTR.lua
@@ -1,4 +1,4 @@
-local DELAY_SEC = 0.250
+local DELAY_SEC = 0.150
 local DELAY_2 = 0.050
 
 local buffs_normal = {
@@ -9,6 +9,11 @@ local buffs_normal = {
 	21850,	-- motw
 	9910,	-- thorns
 	20906,	-- trueshot
+
+	17548,	-- grt shadow prot
+	17546,	-- grt nature prot
+	24382,	-- zanza
+	3593,	-- Elixir of Fortitude
 }
 
 local buffs_world = {
@@ -21,6 +26,71 @@ local buffs_world = {
 	24425,	-- ZG heart
 }
 
+local buffs_druid = {
+	17627,	-- flask: distilled wisdom
+	24363,	-- mageblood potion mp5
+	18194,	-- nightfin soup mp5
+	22790,	-- DMN beer spirit
+}
+
+local buffs_hunter = {
+	17626,	-- flask: titans
+	17538,	-- Elixir of the Mongoose
+	24363,	-- mageblood potion mp5
+	18192,	-- Grilled Squid - Increased Agility 10
+	5665,	-- Fury of the Bogling 1 phys dmg
+	16329,	-- Juju might
+	22789,	-- DMN beer stamina
+}
+
+local buffs_mage = {
+	17628,	-- flask: supreme power
+	18194,	-- nightfin soup mp5
+	24363,	-- mageblood potion mp5
+	17539,	-- Greater Arcane Elixir
+	26276,	-- Elixir of Greater Firepower
+	22790,	-- DMN beer spirit
+}
+
+local buffs_paladin = {
+	17627,	-- flask: distilled wisdom
+	24363,	-- mageblood potion mp5
+	18194,	-- nightfin soup mp5
+	22790,	-- DMN beer spirit
+}
+
+local buffs_priest = buffs_paladin
+local buffs_shaman = buffs_paladin
+
+local buffs_rogue = {
+	17626,	-- flask: titans
+	17538,	-- Elixir of the Mongoose
+	20452,	-- Smoked Desert Dumplings - 20 str
+	5665,	-- Fury of the Bogling 1 phys dmg
+	16329,	-- Juju might
+	12451,	-- Juju power
+	22789,	-- DMN beer stamina
+}
+
+local buffs_warlock = {
+	17628,	-- flask: supreme power
+	18194,	-- nightfin soup mp5
+	24363,	-- mageblood potion mp5
+	17539,	-- Greater Arcane Elixir
+	11474,	-- Elixir of shadowpower
+	22790,	-- DMN beer spirit
+}
+
+local buffs_warrior = {
+	17626,	-- flask: titans
+	17538,	-- Elixir of the Mongoose
+	20452,	-- Smoked Desert Dumplings - 20 str
+	5665,	-- Fury of the Bogling 1 phys dmg
+	16329,	-- Juju might
+	12451,	-- Juju power
+	22789,	-- DMN beer stamina
+}
+
 local buffs_heal = {
 	29166,	-- innervate
 	10929,	-- renew r9
@@ -28,39 +98,16 @@ local buffs_heal = {
 	9858,	-- regrowth r9
 }
 
-local buffs_melee = {
-	17626,	-- flask: titans
-	17538,	-- Elixir of the Mongoose
-	17038,	-- Winterfall Firewater
-	16323,	-- Juju Power
-	18192,	-- Grilled Squid - Increased Agility 10
-}
-
-local buffs_healer = {
-	17627,	-- flask: distilled wisdom
-	24363,	-- mageblood potion mp5
-	18194,	-- nightfin soup mp5
-}
-
-local buffs_caster = {
-	17628,	-- flask: supreme power
-	18194,	-- nightfin soup mp5
-	24363,	-- mageblood potion mp5
-	17539,	-- Greater Arcane Elixir
-	26276,	-- Elixir of Greater Firepower
-	11474,	-- Elixir of Shadow Power
-}
-
 local class_buffs = {
-	["Druid"]	= buffs_healer,
-	["Hunter"]	= buffs_melee,
-	["Mage"]	= buffs_caster,
-	["Paladin"]	= buffs_healer,
-	["Priest"]	= buffs_healer,
-	["Rogue"]	= buffs_melee,
-	["Shaman"]	= buffs_healer,
-	["Warlock"]	= buffs_caster,
-	["Warrior"]	= buffs_melee,
+	["Druid"]	= buffs_druid,
+	["Hunter"]	= buffs_hunter,
+	["Mage"]	= buffs_mage,
+	["Paladin"]	= buffs_paladin,
+	["Priest"]	= buffs_priest,
+	["Rogue"]	= buffs_rogue,
+	["Shaman"]	= buffs_paladin,
+	["Warlock"]	= buffs_shaman,
+	["Warrior"]	= buffs_warrior,
 }
 
 --------------------------------------------------------------------------------
